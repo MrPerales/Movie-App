0ccdc5280df87153ed1c55a387c05b0e024add79
Carlos Perales <carlosperales050@gmail.com>
2023-07-28 19:07:44 -0600

    call api ok

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Card() {
+    return (
+        <div class="movie-container">
+            <img
+                src=""
+                alt="movie Name"
+                class="movie-img"
+            />
+        </div>
+    )
+}
+export default Card
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,28 @@
 import React from "react";
+import { API, API_Bearer, API_KEY } from "secret";
+
+const API_TRENDING=`${API}/trending/movie/day`
+const options={
+    method:'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: API_Bearer
+      }
+    
+}
 
 function HomePage() {
+
+    const [data, setData] = React.useState([])
+
+    React.useEffect(() => {
+        fetch(API_TRENDING, options)
+            .then(resp => resp.json())
+            .then(data => setData(data))
+            .catch(error => console.log(error))
+    }, [])
+    console.log(data);
+
     return (
         <>
             <h1 className="text-orange-700 text-5xl">
