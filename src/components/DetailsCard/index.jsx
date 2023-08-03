fa4f868814566322fa963e38ba4c9bef0d3cea81
Carlos Perales <carlosperales050@gmail.com>
2023-08-03 13:45:46 -0600

    component

---
+++ src/components/DetailsCard/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function DetailCard(){
+
+
+
+
+    return(
+        <>
+            <h1>titleMovie</h1>
+            <span>score</span>
+            <div>
+                categories list
+            </div>
+            <div>
+                <p>description</p>
+            </div>
+        </>
+    )
+}
+
+export default DetailCard;
